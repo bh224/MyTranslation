302e58cbef02d7b93be8747c506ede4ce5969c25
bh224 <ericakim224@gmail.com>
2023-03-25 00:57:47 +0900

    글로서리 marker url 추가

--- projects/urls.py
+++ projects/urls.py
@@ -7,10 +7,15 @@ from . import views
 
 urlpatterns = [
     path("", project_api.Projects.as_view()),
+    path("test", project_api.Tests.as_view()),
+    path("<int:pk>", project_api.ProjectDetail.as_view()),
+    path("<int:pk>/send-email", project_api.SendEmail.as_view()),
     path("<int:pk>/comments", comment_api.Comments.as_view()),
-    path("<int:pk>/categories", category_api.Category.as_view()),
-    path("<int:pk>/glossaries", glossary_api.Glossary.as_view()),
+    path("<int:pk>/categories", category_api.Categories.as_view()),
+    path("<int:pk>/glossaries", glossary_api.Glossaries.as_view()),
+    path("<int:pk>/glossaries/marker", glossary_api.Marker.as_view()),
     path("<int:pk>/translations", translation_api.Translations.as_view()),
     path("<int:pk>/checks", translation_api.Check.as_view()),
+    path("upload/project", project_api.ProjectMembers.as_view()),
 
 ]
